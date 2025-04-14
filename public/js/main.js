455d2624c87ad04c6f28383c388f05f86221b480
igorG7 <giovane167@outlook.com>
2025-04-14 15:46:26 -0300

    Alternador do pop-up update

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 const menuButton = document.querySelector(".btn-menu");
 const sideMenu = document.querySelector(".side-menu");
 const itemsMenu = document.querySelectorAll(".item-menu");
+const editButton = document.querySelector(".edit-button");
+const popUpUpdate = document.querySelector(".update-container");
 
 menuButton.addEventListener("click", (e) => {
   if (!sideMenu.classList.contains("active")) {
@@ -12,3 +14,22 @@ menuButton.addEventListener("click", (e) => {
   sideMenu.classList.remove("active");
   itemsMenu.forEach((item) => item.classList.remove("active"));
 });
+
+editButton.addEventListener("click", () => {
+  if (!popUpUpdate.classList.contains("active"))
+    popUpUpdate.classList.add("active");
+});
+
+popUpUpdate.addEventListener("click", (e) => showPopup(e, popUpUpdate));
+
+const showPopup = (e, popup) => {
+  console.log(e.target);
+
+  const classToClose = ["update-container", "close", "cancel-button"];
+
+  console.log(classToClose.includes(e.target.classList[0]));
+
+  if (classToClose.includes(e.target.classList[0])) {
+    popup.classList.remove("active");
+  }
+};
