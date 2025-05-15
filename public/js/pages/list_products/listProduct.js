aac731b2d385cd5eae508369b762a1e11189ddcb
igorG7 <giovane167@outlook.com>
2025-05-15 17:04:19 -0300

    Filtragem através da barra de pesquisa

---
+++ public/js/pages/list_products/listProduct.js
@@ -0,0 +1,36 @@
+import { Card } from "./productCard.js";
+
+const getProducts = async (event) => {
+  try {
+    const list = document.querySelector(".products");
+    list.innerHTML = "";
+
+    const value = event?.target.value;
+    const response = await axios.get(query(value));
+
+    const products = response.data.data;
+    products.forEach((data) => new Card(data));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const query = (value = "") => {
+  const query = `products?productName=${value}&providerName=${value}&makerName=${value}`;
+  return query;
+};
+
+const debounce = (func, delay) => {
+  let timeout;
+
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+};
+
+getProducts();
+const search = document.querySelector(".search-input");
+search.addEventListener("input", debounce(getProducts, 1000));
