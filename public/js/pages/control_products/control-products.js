2c59e03a1dda25009d18acb7ca09305798f80621
igorG7 <giovane167@outlook.com>
2025-05-22 16:10:34 -0300

    Adicionando pop-up para confirmação da deleção de produto

--- public/js/pages/control_products/control-products.js
+++ public/js/pages/control_products/control-products.js
@@ -2,6 +2,26 @@ import { ValidForm } from "../../util/form/validForm.js";
 
 const params = new URLSearchParams(window.location.search);
 const id = params.get("_id");
-console.log(id);
+const body = document.querySelector("body");
+const popUpDelete = document.querySelector(".delete-container");
 
 const form = new ValidForm(id);
+
+const deleteButton = document.querySelector(".delete-button1");
+
+deleteButton.addEventListener("click", () => {
+  if (!popUpDelete.classList.contains("active"))
+    popUpDelete.classList.add("active");
+  body.classList.add("lock");
+});
+
+popUpDelete.addEventListener("click", (e) => showPopup(e, popUpDelete));
+
+const showPopup = (e, popup) => {
+  const classToClose = ["close", "cancel-btn", "delete-container"];
+
+  if (classToClose.includes(e.target.classList[0])) {
+    popup.classList.remove("active");
+    body.classList.remove("lock");
+  }
+};
