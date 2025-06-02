fb3deba9c81ea1aa3ea6c264e042673f69453f34
igorG7 <giovane167@outlook.com>
2025-06-02 14:52:57 -0300

    Controle de acesso ao dashboard

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,7 @@
 exports.home = (req, res) => {
+  const user = res.locals.user;
+  if (!user) return res.redirect("/login-administrator");
+  if (user.role !== "admministrator")
+    return res.status(403).json({ message: "Erro 403: Acesso negado." });
   res.render("index");
 };
