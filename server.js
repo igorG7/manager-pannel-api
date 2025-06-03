c98e002b8c2291be96a2cb85f367aa1740bf9bde
igorG7 <giovane167@outlook.com>
2025-06-03 15:42:45 -0300

    Adicionando envio apenas por http(s) das sessions

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(
     secret: "user-dash",
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // secure: true
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true }, // secure: true
   })
 );
 
