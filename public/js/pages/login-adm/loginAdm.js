133ecb7e4d02a302afc36816ec090945f3779e8b
igorG7 <giovane167@outlook.com>
2025-06-03 14:50:08 -0300

    Criação da Classe de login e adição no script principal

--- public/js/pages/login-adm/loginAdm.js
+++ public/js/pages/login-adm/loginAdm.js
@@ -1,8 +1,12 @@
 import { toggleVisibilityPass } from "../../util/ui/toggleVisibilityPass.js";
+import { Login } from "../../util/auth-form/LoginClass.js";
 
 const inputPass = document.querySelector(".input");
 const showPass = document.querySelector(".eye");
 const hidePass = document.querySelector(".eye-off");
+const form = document.querySelector(".form-login-container");
+
+const formLogin = new Login();
 
 hidePass.addEventListener("click", (e) =>
   toggleVisibilityPass(e, hidePass, showPass, inputPass, "text")
@@ -11,3 +15,8 @@ hidePass.addEventListener("click", (e) =>
 showPass.addEventListener("click", (e) =>
   toggleVisibilityPass(e, showPass, hidePass, inputPass, "password")
 );
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  formLogin.handleLogin();
+});
