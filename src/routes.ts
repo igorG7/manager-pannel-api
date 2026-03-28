b2dbe343d5ea8f3f3c7689f6a44c03bda42c40f1
igorG7 <igor.giovane.costa@gmail.com>
2026-03-28 18:39:48 -0300

    Implementando rota de usuários no arquivo raiz das rotas

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
 
+import userRoutes from "./modules/user/user-routes.ts";
+
 const routes = Router();
+
+routes.use("/user", userRoutes);
 
 export default routes;
