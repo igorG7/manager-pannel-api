3f02dbcdeecf372a9c3098471e69759a6c959a49
igorG7 <igor.giovane.costa@gmail.com>
2026-03-28 19:57:54 -0300

    Removendo arquivos antigos

--- public/js/util/ui/toggleVisibilityPass.js
+++
@@ -1,8 +0,0 @@
-export const toggleVisibilityPass = (e, visible, hidden, input, attribute) => {
-  if (e.target.classList.contains("on")) {
-    visible.classList.remove("on");
-    hidden.classList.add("on");
-  }
-
-  input.setAttribute("type", attribute);
-};
