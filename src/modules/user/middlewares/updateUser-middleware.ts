aedb01ea6a2e2c12462e56fc74900b2bfa9680c9
igorG7 <igor.giovane.costa@gmail.com>
2026-03-28 18:54:04 -0300

    Implementação de middlewares de validação para os respectivos controllers existentes.

---
+++ src/modules/user/middlewares/updateUser-middleware.ts
@@ -0,0 +1,24 @@
+import type { Request, Response, NextFunction } from "express";
+import type { IUser } from "../domain/user-interface.ts";
+import { Key } from "../../shared/utils/validations/key.ts";
+
+export const updateUser = (req: Request, res: Response, next: NextFunction) => {
+  const body: Partial<IUser> = req.body;
+  const id = req.params.id;
+
+  if (!id) throw new Error("Id de usuário não fornecido.");
+
+  const keys = Object.keys(body);
+  if (!keys.length) throw new Error("Nenhuma informação para cadastro foi enviada.");
+
+  const validators = {
+    email: { type: "string", required: false },
+    name: { type: "string", required: false },
+    role: { type: "string", required: false, enum: ["ADMIN", "OPERATOR", "SELLER"] },
+    active: { type: "boolean", required: false },
+  };
+
+  Key.validate(validators, body);
+
+  next();
+};
