84aa9131774eed002f8a94b266325ec7085c19cb
igorG7 <igor.giovane.costa@gmail.com>
2026-04-11 16:22:48 -0300

    (refactor/base-architecture-modules) Implementado rota referente a entidade “Product” no arquivo raiz de rotas da aplicação.

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import userRoutes from "./modules/user/user-routes.ts";
+import productRoutes from "./modules/products/product-routes.ts";
 
 const routes = Router();
 
 routes.use("/user", userRoutes);
+routes.use("/product", productRoutes);
 
 export default routes;
