64374b1e49cbb4442e5049edfc41a89cdef043d9
igorG7 <igor.giovane.costa@gmail.com>
2026-04-11 16:24:36 -0300

    (refactor/base-architecture-modules) Melhoria na classe de validação Key. Adição de novas verificações para ampliar a cobertura de validação dos dados vindos das requisições.

--- src/modules/shared/utils/validations/key.ts
+++ src/modules/shared/utils/validations/key.ts
@@ -1,5 +1,9 @@
+import { verifyMinEntry } from "./minEntry.ts";
+
 export class Key {
   static validate(validators: any, data: any) {
+    verifyMinEntry(data);
+
     for (const key of Object.keys(data)) {
       if (!validators[key]) {
         throw new Error(`Chave '${key}' não permitida`);
@@ -18,7 +22,11 @@ export class Key {
         continue;
       }
 
-      if (typeof value !== validator.type) {
+      if (validator.type === "array") {
+        if (!Array.isArray(value)) {
+          throw new Error(`Chave '${key}' precisa ser do tipo ${validator.type}.`);
+        }
+      } else if (typeof value !== validator.type) {
         throw new Error(`Chave '${key}' precisa ser do tipo ${validator.type}.`);
       }
 
@@ -26,11 +34,29 @@ export class Key {
         throw new Error(`Chave '${key}' sem valor válido.`);
       }
 
-      if (validator.enum) Key.enum(validator, value, key);
-    }
-  }
+      if (validator.enum && !validator.enum.includes(value)) {
+        throw new Error(`Chave "${key}" permite apenas: ${validator.enum.join(", ")}.`);
+      }
 
-  static enum(validator: any, value: any, key: string) {
-    if (!validator.enum.includes(value)) throw new Error(`Chave "${key}" não permite esse valor.`);
+      if (validator.type === "array" && validator.items) {
+        for (const item of value) {
+          if (typeof item !== validator.items) {
+            throw new Error(`Itens de '${key}' precisam ser do tipo ${validator.items}.`);
+          }
+        }
+      }
+
+      if (validator.fields) {
+        if (validator.type === "object") {
+          Key.validate(validator.fields, value);
+        }
+
+        if (validator.type === "array") {
+          for (const item of value) {
+            Key.validate(validator.fields, item);
+          }
+        }
+      }
+    }
   }
 }
